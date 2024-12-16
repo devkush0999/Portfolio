09891d09227c0ff277a99ec5eddf50d9ed37802d
devkush0999 <devesh2cse@gmail.com>
2024-12-16 11:55:03 +0530

    update resume link

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/devkush0999",
   resume:
-    "https://mega.nz/file/gyNnSJDA#MDl9bKYJ2om_d3WWvbbeF5cAatSnq3APA5q5Qvt1VMQ",
+    "https://drive.google.com/file/d/1o7rtxTLIkVRWxQdk8PI-DhTjYut8Q5q_/view?usp=drivesdk",
   linkedin: "https://www.linkedin.com/in/devesh-kumar-singh-3b1863196/",
   twitter: "https://twitter.com/Deveshs80242433",
   insta: "https://www.instagram.com/devesh911999/",
